cc497d67e784e2d66f8820f6ca64efe0d8be344f
PronabM <pronab.mukherjee.96@gmail.com>
2021-03-11 00:30:25 +0530

    Removed Umlaut to prevent RTE

--- vitals/MonitorStates.java
+++ vitals/MonitorStates.java
@@ -3,8 +3,8 @@ package vitals;
 public enum MonitorStates {
 	WARNING("WARNING:: ", "WARNUNG:: "),
 	BREACH("BREACH:: ", "BRUCH:: "),
-	IS_ABOVE_ALLOWED_LIMIT(" is above the allowed limit.", " liegt über dem zulässigen Grenzwert."),
-	IS_BELOW_ALLOWED_LIMIT(" is below the allowed limit.", " liegt unter dem zulässigen Grenzwert."),
+	IS_ABOVE_ALLOWED_LIMIT(" is above the allowed limit.", " liegt uber dem zulassigen Grenzwert."),
+	IS_BELOW_ALLOWED_LIMIT(" is below the allowed limit.", " liegt unter dem zulassigen Grenzwert."),
 	IS_IN_LOWER_WARNING(" is reaching the lower limit.", " erreicht die Untergrenze."),
 	IS_IN_UPPER_WARNING(" is reaching the upper limit.", " erreicht die Obergrenze."),
 	IS_WITHIN_LIMIT(" is within the limit.", " ist innerhalb der Grenze.");
